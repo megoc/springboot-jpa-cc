ebf0530ddbdc62c9bfcec36e5c7612d5cb806efc
flymegoc <343672271@qq.com>
2017-10-29 19:45:01 +0800

    add news picture

---
+++ src/main/java/com/flymegoc/cc/ServletInitializer.java
@@ -0,0 +1,19 @@
+package com.flymegoc.cc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created by flymegoc on 2017/4/8.
+ */
+public class ServletInitializer extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootJpaCcApplication.class);
+    }
+}
