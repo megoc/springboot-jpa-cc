676690033ca5c7e38e10c6d29ce7db08d6dd0750
flymegoc <343672271@qq.com>
2017-11-02 15:01:21 +0800

    add news picture

--- src/main/java/com/flymegoc/cc/config/MyWebAppConfigurer.java
+++ src/main/java/com/flymegoc/cc/config/MyWebAppConfigurer.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+//                .allowedOrigins("http://192.168.1.97")
+//                .allowedMethods("GET", "POST")
+                .allowCredentials(true).maxAge(3600);
     }
 }
